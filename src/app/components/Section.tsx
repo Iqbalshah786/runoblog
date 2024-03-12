70959c5717322146ae791c2ac491a24fa911c449
Iqbal Shah Nadiri <iqbalshahnadiri786@gmail.com>
2024-03-12 16:00:25 +0500

    Update Section component styling

--- src/app/components/Section.tsx
+++ src/app/components/Section.tsx
@@ -16,12 +16,12 @@ export function Section({ posts }: Props) {
     { title: "Branding" },
   ];
   return (
-    <section className=" w-full mt-24 px-[4vw]">
-      <h1 className="text-[#495057]  font-bold text-[1.875vw] my-6">
+    <section className=" w-full mt-16 md:mt-24 px-[4vw]">
+      <h1 className="text-[#495057]  font-bold text-[1rem] md:text-[1.875vw] my-6">
         Popular topics
       </h1>
       <div className="flex flex-col w-full ">
-        <ul className="flex gap-4  text-xs lg:text-lg font-bold flex-shrink-0 mb-6">
+        <ul className="flex gap-[10px] sm:gap-4  text-[12px] sm:text-xs lg:text-lg font-bold flex-shrink-0 mb-6">
           {categories.map((category) => (
             <li key={category.title}>
               <Link href={"/"}>{category.title}</Link>
